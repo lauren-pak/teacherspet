4bdc466f5a356df7fd94670b52dc9aad4065cde2
Yogurt:) <153968747+Jealous7@users.noreply.github.com>
2026-01-18 01:30:46 +0800

    kk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="teacherspet",
-    version="0.1.0",
+    version="0.0.0",
     packages=find_packages(),
     install_requires=[
         "ElevenLabs",
@@ -14,8 +14,6 @@ setup(
 
         "opencv-python"
 
-        "win32gui",
-        "win32con"
 
     ],
 )
