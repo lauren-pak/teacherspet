9dca1a37a92392f65e0f6fd141832d21cdf629a8
Yogurt:) <153968747+Jealous7@users.noreply.github.com>
2026-01-17 16:48:01 +0800

    push5

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import cv2 as cv
 from ultralytics import YOLO
 from collections import deque
-from PySide6 import QtWidgets, QtCore, QtGui
-import sys, random, platform, threading
-import pyautogui
 import time
-import subprocess
-from urllib.parse import urlparse
+# import files
+from camera import Camera
 
+print("hello1")
 def teachersPet():
     print("hello")
+    new_camera = Camera()
+    new_camera.run()
+
+    #wait initialisation time
+    time.sleep(10)
+
+
+teachersPet()
