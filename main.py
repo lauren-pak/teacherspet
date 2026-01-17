0925e70bec2b4975fcced17bf80970a7ffb27081
Yogurt:) <153968747+Jealous7@users.noreply.github.com>
2026-01-17 15:44:59 +0800

    push4

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+from ultralytics import YOLO
+from collections import deque
+from PySide6 import QtWidgets, QtCore, QtGui
+import sys, random, platform, threading
+import pyautogui
+import time
+import subprocess
+from urllib.parse import urlparse
+
+def teachersPet():
+    print("hello")
